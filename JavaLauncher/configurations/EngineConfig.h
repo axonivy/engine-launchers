f5ea4621e00ef5f5800eb18ffa50fe67728aaa5f
Peter Stöckli <peter.stoeckli@axonivy.com>
2016-07-08 13:46:52 +0000

    XIVY-1051: ServerConfigurationTool should be an official tool - add ServerConfigurationTool as windows exe called EngineConfigC.exe reviewed by nobody

---
+++ JavaLauncher/configurations/EngineConfig.h
@@ -0,0 +1,19 @@
+/*
+ * WebAppC Configuration
+ */
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define APPICON "icons\\AxonIvyEngineConfig.ico"
+
+CLaunchConfiguration getLaunchConfiguration()
+{
+	CLaunchConfiguration config;
+
+	config = CLaunchConfiguration::getServerApplicationDefault();
+	config.setApplicationName("EngineConfig");
+	config.setMainJavaClass("ch.ivyteam.ivy.server.configuration.ServerConfigurationTool");
+	return config;
+}
+
+#endif
