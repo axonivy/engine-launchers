09f40af463138e1566ce565d0602dd0b43a4ec19
Alexander Suter <alexander.suter@axonivy.com>
2019-09-02 16:17:56 +0200

    XIVY-2950 Remove xms and lower xmx for low memory jvm settings

--- JvmLauncher/JavaProgram.cpp
+++ JvmLauncher/JavaProgram.cpp
@@ -394,8 +394,7 @@ void CJavaProgram::initializeVmOptions(CVmOptions& options, LPCSTR pcApplication
 		// this also prevents e.g. starting management port
 		// when stoping the axon ivy engine
 		options.addOption("-XX:-OmitStackTraceInFastThrow", NULL);
-		options.addOption("-Xmx768m", NULL);
-		options.addOption("-Xms128m", NULL);
+		options.addOption("-Xmx256m", NULL);
 	}
 	else
 	{
